e2461853fd141a5e58fc5d9879906b65f53055e6
Facundo-Nastasi-DAW <facundonastasi@paucasesnovescifp.cat>
2024-10-07 15:17:58 +0200

    ro4 revision

--- 1r trim/tema1/Trabajos/Piano/scripts/musihacks.js
+++ 1r trim/tema1/Trabajos/Piano/scripts/musihacks.js
@@ -1,39 +1,46 @@
-var partituras = [
-    {nom: 'Himno de la alegría', 
-        partitura:{ notas:[
-                {nom: 'Si', tipus: 'natural'},
-                {nom: 'Si', tipus: 'natural'},
-                {nom: 'Do', tipus: 'natural'},
-                {nom: 'Re', tipus: 'natural'},
-                {nom: 'Re', tipus: 'natural'},
-                {nom: 'Do', tipus: 'natural'},
-                {nom: 'Si', tipus: 'natural'},
-                {nom: 'Sol', tipus: 'natural'},
-                {nom: 'Sol', tipus: 'natural'},
-                {nom: 'La', tipus: 'natural'},
-                {nom: 'Si', tipus: 'natural'},
-                {nom: 'Si', tipus: 'natural'},
-                {nom: 'La', tipus: 'natural'},
-            ]
-        }
-    },
-    {}
-];
-
 class Nota {
     constructor(nom, tipus) {
         this.nom = nom;
         this.tipus = tipus;
     }
 
+    equals(nota) {
+        return this.nom == nota.nom && this.tipus == nota.tipus;
+    }
+
     /*
-        constructor(nom, tipus, tiempo) {
+        constructor(nom, tipus, tempo) {
             this.nom = nom;
             this.tipus = tipus;
-            this.tiempo = tiempo;
+            this.tempo = tempo;
         }
     */
 }
+
+const partitures = [
+    {   
+        nom: 'Himno de la alegría', 
+        notes:[
+            new Nota('Si', 'natural'),
+            new Nota('Si', 'natural'),
+            new Nota('Do', 'natural'),
+            new Nota('Re', 'natural'),
+            new Nota('Re', 'natural'),
+            new Nota('Do', 'natural'),
+            new Nota('Si', 'natural'),
+            new Nota('La', 'natural'),
+            new Nota('Sol', 'natural'),
+            new Nota('Sol', 'natural'),
+            new Nota('La', 'natural'),
+            new Nota('Si', 'natural'),
+            new Nota('Si', 'natural'),
+            new Nota('La', 'natural'),
+            {nom:'Re', estat:'natural'},
+            {nom:'Do', estat:'natural'},
+            {nom:'Si', estat:'natural'},
+        ]
+    }
+];
 
 function addCerca(nom, tipus, cerca) {
     cerca.push(new Nota(nom, tipus));
@@ -41,10 +48,13 @@ function addCerca(nom, tipus, cerca) {
 
 function cercador(cerca){
     resultat = [];
-    for (let partitura of partituras){
-        if (partitura.notas.includes(cerca)){
+    for (let partitura of partitures){
+        console.log(partitura.notes);
+        console.log(cerca);
+        if (partitura.notes.includes(cerca)){ // no se mete al if. Por algun motivo, al crear notas identicas y compararlas, el método includes las declara distintas.
             resultat.push(partitura);
         }
+        /* Para comparar con el metodo equals de la clase Nota, deberia hacer una clase Partitura, con un método contains, que simule el funcionamiento de includes */
     }
     console.log(resultat);
 }
@@ -59,6 +69,5 @@ function pruebas(){
     for (let nota of muestra){
         addCerca(nota.nom, nota.tipus, cerca);
     }
-
     cercador(cerca);
 }
