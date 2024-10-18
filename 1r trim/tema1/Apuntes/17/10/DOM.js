d4be129d18f1ccd43ff13c70d2af3ac2482f2236
Facundo-Nastasi-DAW <facundonastasi@paucasesnovescifp.cat>
2024-10-18 17:25:40 +0200

    apuntes

---
+++ 1r trim/tema1/Apuntes/17/10/DOM.js
@@ -0,0 +1,18 @@
+var v2 = document.getElementsByName("sexo");
+console.log("getElementsByName('sexo')", v2)
+
+var V3 = document.getElementsByTagName("input")
+console.log("getElementsByTagName('input')", V3)
+
+var v4 = document.getElementsByClassName("sexo");
+console.log('document.getElementsByClassName("sexo")', v4)
+
+var v0 = document.getRootNode();
+
+var v1 = document.getElementsByTagName("body");
+
+console.log(v1[0]);
+console.log(v1[0].childNodes);
+console.log(v1[0].children);
+console.log(v1[0].parentElement);
+console.log(v1[0].children[0].nextSibling);
